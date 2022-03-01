f90b9e1d978199d4497ffc4026ffc47d04bddd2f
banjjak2 <interr0bang@naver.com>
2022-03-01 16:16:00 +0900

    fix: 0xFF000000를 or 연산

--- app/src/main/java/com/example/kotlin_drawingapp/shapemodel/Color.kt
+++ app/src/main/java/com/example/kotlin_drawingapp/shapemodel/Color.kt
@@ -14,6 +14,6 @@ data class Color(val r: Int, val g: Int, val b: Int) {
         val red = r shl 16 and 0x00FF0000 //Shift red 16-bits and mask out other stuff
         val green = g shl 8 and 0x0000FF00 //Shift Green 8-bits and mask out other stuff
         val blue = b and 0x000000FF //Mask out anything not blue.
-        return red or green or blue //0xFF000000 for 100% Alpha. Bitwise OR everything together.
+        return -0x1000000 or red or green or blue //0xFF000000 for 100% Alpha. Bitwise OR everything together.
     }
 }
