113a2e03694159878aa55f7938c99ad72edfbbee
banjjak2 <interr0bang@naver.com>
2022-03-01 14:33:43 +0900

    feat: RGB를 int로 변환하는 코드 추가

--- app/src/main/java/com/example/kotlin_drawingapp/shapemodel/Color.kt
+++ app/src/main/java/com/example/kotlin_drawingapp/shapemodel/Color.kt
@@ -9,4 +9,11 @@ data class Color(val r: Int, val g: Int, val b: Int) {
             throw Exception("0~255만 가능합니다.")
         }
     }
+
+    fun getRgb(): Int {
+        val red = r shl 16 and 0x00FF0000 //Shift red 16-bits and mask out other stuff
+        val green = g shl 8 and 0x0000FF00 //Shift Green 8-bits and mask out other stuff
+        val blue = b and 0x000000FF //Mask out anything not blue.
+        return red or green or blue //0xFF000000 for 100% Alpha. Bitwise OR everything together.
+    }
 }
