d900acfad46c3a16f3da36da36256f3f76053bcb
hwj <jminie@naver.com>
2022-03-01 19:39:49 +0900

    feat : SquareModelFactory 리팩토링

--- app/src/main/java/com/example/kotlin_drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_drawingapp/MainActivity.kt
@@ -10,44 +10,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val squareModelFactory = SquareModelFactory()
-        Log.d("Rect1", "(${squareModelFactory.createSquareObject()[0]}), " +
-                "X: ${squareModelFactory.createSquareObject()[1]}, " +
-                "Y: ${squareModelFactory.createSquareObject()[2]}, " +
-                "W ${squareModelFactory.createSquareObject()[3]}, " +
-                "H: ${squareModelFactory.createSquareObject()[4]}, " +
-                "R: ${squareModelFactory.createSquareObject()[5]}, " +
-                "G: ${squareModelFactory.createSquareObject()[6]}, " +
-                "B: ${squareModelFactory.createSquareObject()[7]}, " +
-                "Alpha: ${squareModelFactory.createSquareObject()[8]} ")
-
-        Log.d("Rect2", "(${squareModelFactory.createSquareObject()[0]}), " +
-                "X: ${squareModelFactory.createSquareObject()[1]}, " +
-                "Y: ${squareModelFactory.createSquareObject()[2]}, " +
-                "W ${squareModelFactory.createSquareObject()[3]}, " +
-                "H: ${squareModelFactory.createSquareObject()[4]}, " +
-                "R: ${squareModelFactory.createSquareObject()[5]}, " +
-                "G: ${squareModelFactory.createSquareObject()[6]}, " +
-                "B: ${squareModelFactory.createSquareObject()[7]}, " +
-                "Alpha: ${squareModelFactory.createSquareObject()[8]} ")
-
-        Log.d("Rect3", "(${squareModelFactory.createSquareObject()[0]}), " +
-                "X: ${squareModelFactory.createSquareObject()[1]}, " +
-                "Y: ${squareModelFactory.createSquareObject()[2]}, " +
-                "W ${squareModelFactory.createSquareObject()[3]}, " +
-                "H: ${squareModelFactory.createSquareObject()[4]}, " +
-                "R: ${squareModelFactory.createSquareObject()[5]}, " +
-                "G: ${squareModelFactory.createSquareObject()[6]}, " +
-                "B: ${squareModelFactory.createSquareObject()[7]}, " +
-                "Alpha: ${squareModelFactory.createSquareObject()[8]} ")
-
-        Log.d("Rect4", "(${squareModelFactory.createSquareObject()[0]}), " +
-                "X: ${squareModelFactory.createSquareObject()[1]}, " +
-                "Y: ${squareModelFactory.createSquareObject()[2]}, " +
-                "W ${squareModelFactory.createSquareObject()[3]}, " +
-                "H: ${squareModelFactory.createSquareObject()[4]}, " +
-                "R: ${squareModelFactory.createSquareObject()[5]}, " +
-                "G: ${squareModelFactory.createSquareObject()[6]}, " +
-                "B: ${squareModelFactory.createSquareObject()[7]}, " +
-                "Alpha: ${squareModelFactory.createSquareObject()[8]} ")
+        Log.d("Rect1", squareModelFactory.createLogMessage())
+        Log.d("Rect2", squareModelFactory.createLogMessage())
+        Log.d("Rect3", squareModelFactory.createLogMessage())
+        Log.d("Rect4", squareModelFactory.createLogMessage())
     }
 }
--- app/src/main/java/com/example/kotlin_drawingapp/SquareModelFactory.kt
+++ app/src/main/java/com/example/kotlin_drawingapp/SquareModelFactory.kt
@@ -8,7 +8,7 @@ class SquareModelFactory {
     private val arrayOfIdSeparate = arrayOf(firstIdSeparate ,secondIdSeparate ,thirdIdSeparate)
     private val idResult = arrayOfIdSeparate.joinToString("-")
 
-    fun createSquareObject(): Array<String>  {
+    private fun createSquareObject(): Array<String>  {
         return arrayOf(
             idResult,
             squareModel.createSize()[0],
@@ -19,5 +19,17 @@ class SquareModelFactory {
             squareModel.createRGB()[1],
             squareModel.createRGB()[2],
             squareModel.createAlpha())
+    }
+
+    fun createLogMessage(): String {
+        return "(${createSquareObject()[0]}), " +
+                "X: ${createSquareObject()[1]}, " +
+                "Y: ${createSquareObject()[2]}, " +
+                "W ${createSquareObject()[3]}, " +
+                "H: ${createSquareObject()[4]}, " +
+                "R: ${createSquareObject()[5]}, " +
+                "G: ${createSquareObject()[6]}, " +
+                "B: ${createSquareObject()[7]}, " +
+                "Alpha: ${createSquareObject()[8]} "
     }
 }
