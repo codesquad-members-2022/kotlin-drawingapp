c01e40d70fcad037ae40a04d7a10256ace13f35b
ramkid91 <ramkid91@gmail.com>
2022-03-02 01:37:11 +0900

    feat:Add show selected Recctangle RGB and Opacity

--- app/src/main/java/view/MainActivity.kt
+++ app/src/main/java/view/MainActivity.kt
@@ -2,43 +2,48 @@ package view
 
 import Rect
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.ImageView
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.codesquad.kotlin_drawingapp.R
 import presenter.MainPresenter
 
 class MainActivity : AppCompatActivity(), MainContract.View {
     private lateinit var mainLayout: FrameLayout
-    private lateinit var presenter:MainPresenter
+    private lateinit var presenter: MainPresenter
     private var selectedBorder: ImageView? = null
-    private var selectedRectangleView:ImageView?=null
+    private var selectedRectangleView: ImageView? = null
+
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        presenter= MainPresenter(this,this, resources.displayMetrics.density)
-        mainLayout= findViewById(R.id.image_container)
-        val btnMakeRectangle= findViewById<Button>(R.id.btn_addRectangle)
+        presenter = MainPresenter(this, this, resources.displayMetrics.density)
+        mainLayout = findViewById(R.id.image_container)
+        val btnMakeRectangle = findViewById<Button>(R.id.btn_addRectangle)
         btnMakeRectangle.setOnClickListener {
             mainLayout.addView(presenter.createRectangle())
         }
 
         mainLayout.setOnTouchListener { _, motionEvent ->
             mainLayout.removeView(selectedBorder)
-            selectedBorder=null
+            selectedBorder = null
             presenter.selectRectangle(motionEvent.x, motionEvent.y)
             true
         }
     }
 
     override fun showSelected(rect: Rect, rectView: ImageView?, border: ImageView?) {
-        selectedBorder=border
-        selectedRectangleView=rectView
-        mainLayout.addView(border)
+        selectedBorder = border
+        selectedRectangleView = rectView
+        rectView?.let {
+            val rgbValueTextView= findViewById<TextView>(R.id.tv_rgb_value)
+            val opacitySeekBar =findViewById<SeekBar>(R.id.seekbar_opacity)
+            rgbValueTextView.text = rect.backGroundColor.getRGBHexValue()
+            opacitySeekBar.progress = rect.getOpacity()
+            mainLayout.addView(border)
+        }
     }
 
 }
--- app/src/main/kotlin/BackGroundColor.kt
+++ app/src/main/kotlin/BackGroundColor.kt
@@ -8,4 +8,8 @@ data class BackGroundColor(val redValue:Int, val greenValue:Int, val blueValue:I
     fun getBackGroundColor(): Int {
         return Color.rgb(redValue,greenValue,blueValue)
     }
+
+    fun getRGBHexValue():String{
+        return "#${Integer.toHexString(redValue)}${Integer.toHexString(greenValue)}${Integer.toHexString(blueValue)}"
+    }
 }
