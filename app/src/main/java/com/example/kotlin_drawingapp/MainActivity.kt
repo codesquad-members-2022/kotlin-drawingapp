690c42a8f35d74703084ec7bb2bbd4e392611705
banjjak2 <interr0bang@naver.com>
2022-03-03 20:06:27 +0900

    feat: 메인 액티비티의 showRectangle 구현

--- app/src/main/java/com/example/kotlin_drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_drawingapp/MainActivity.kt
@@ -1,8 +1,42 @@
 package com.example.kotlin_drawingapp
 
+import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlin_drawingapp.model.Color
+import com.example.kotlin_drawingapp.model.Rectangle
+
 private const val TAG = "MainActivity"
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
 
+    private lateinit var presenter: MainContract.Presenter
+    private lateinit var drawView: DrawView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        presenter = MainPresenter(this)
+        drawView = findViewById(R.id.drawView)
+        val btnGenerateRectangle = findViewById<Button>(R.id.btn_generate_rect)
+        btnGenerateRectangle.setOnClickListener {
+            presenter.drawRectangle()
+        }
+    }
+
+    override fun showRectangle(rectangle: Rectangle) {
+        drawView.drawRectangle(rectangle)
+    }
+
+    override fun showRectangleInfo(color: Color, alpha: Int) {
+
+    }
+
+    override fun showRectangleBorder(border: Rectangle) {
+
+    }
+
+    override fun setRectangleAlpha(index: Int, alpha: Int) {
+
+    }
 }
