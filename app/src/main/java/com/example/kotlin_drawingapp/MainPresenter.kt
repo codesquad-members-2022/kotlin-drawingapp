f33bdf2e4b0f487e06fb1549df2d90eb7d810eec
banjjak2 <interr0bang@naver.com>
2022-03-03 20:04:22 +0900

    feat: MainPresenter의 drawRectangle 구현

---
+++ app/src/main/java/com/example/kotlin_drawingapp/MainPresenter.kt
@@ -0,0 +1,25 @@
+package com.example.kotlin_drawingapp
+
+import com.example.kotlin_drawingapp.model.Rectangle
+
+class MainPresenter(
+    private val mainView: MainContract.View
+) : MainContract.Presenter {
+
+    private val plane = Plane()
+    override fun drawRectangle() {
+        mainView.showRectangle(plane.createRectangle())
+    }
+
+    override fun selectRectangle(x: Float, y: Float) {
+
+    }
+
+    override fun setRectangleAlpha(index: Int, alpha: Int) {
+
+    }
+
+    override fun getRectangleInfo(index: Int) {
+
+    }
+}
