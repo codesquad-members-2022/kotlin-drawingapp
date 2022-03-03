830510e4067f73a735200d897db8df0e9e4661b9
banjjak2 <interr0bang@naver.com>
2022-03-03 19:22:08 +0900

    feat: MVP 인터페이스 생성

---
+++ app/src/main/java/com/example/kotlin_drawingapp/MainContract.kt
@@ -0,0 +1,26 @@
+package com.example.kotlin_drawingapp
+
+import com.example.kotlin_drawingapp.model.Color
+import com.example.kotlin_drawingapp.model.Rectangle
+
+class MainContract {
+    interface View {
+        fun showRectangle(rectangle: Rectangle)
+
+        fun showRectangleInfo(color: Color, alpha: Int)
+
+        fun showRectangleBorder(border: Rectangle)
+
+        fun setRectangleAlpha(index: Int, alpha: Int)
+    }
+
+    interface Presenter {
+        fun drawRectangle(): Rectangle
+
+        fun selectRectangle(x: Float, y: Float)
+
+        fun setRectangleAlpha(index: Int, alpha: Int)
+
+        fun getRectangleInfo(index: Int)
+    }
+}
