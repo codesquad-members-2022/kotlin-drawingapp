9c1bed9869c1bbd4fb4c677420764f0d6974db70
banjjak2 <interr0bang@naver.com>
2022-03-04 01:50:09 +0900

    feat: 메인에서 커스텀뷰의 이벤트 핸들러에 등록

--- app/src/main/java/com/example/kotlin_drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_drawingapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.kotlin_drawingapp
 
+import android.graphics.PointF
 import android.os.Bundle
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin_drawingapp.model.Color
 import com.example.kotlin_drawingapp.model.Rectangle
@@ -12,16 +15,26 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 
     private lateinit var presenter: MainContract.Presenter
     private lateinit var drawView: DrawView
+    private lateinit var tvRgb: TextView
+    private lateinit var seekBarAlpha: SeekBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         presenter = MainPresenter(this)
         drawView = findViewById(R.id.drawView)
+        tvRgb = findViewById(R.id.tv_background_color)
+        seekBarAlpha = findViewById(R.id.seekBar)
         val btnGenerateRectangle = findViewById<Button>(R.id.btn_generate_rect)
         btnGenerateRectangle.setOnClickListener {
             presenter.drawRectangle()
         }
+
+        drawView.setOnRectangleClickListener(object : DrawView.SetOnDrawViewTouchListener {
+            override fun onClick(point: PointF) {
+                presenter.selectRectangle(point.x, point.y)
+            }
+        })
     }
 
     override fun showRectangle(rectangles: List<Rectangle>) {
