76e382583c14a0c871415c11ed1a22f991223037
ramkid91 <ramkid91@gmail.com>
2022-03-04 19:05:43 +0900

    feat: Add permission to Access Gallery

--- app/src/main/java/view/MainActivity.kt
+++ app/src/main/java/view/MainActivity.kt
@@ -3,15 +3,21 @@ package view
 import MainPresenter
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.widget.*
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import com.codesquad.kotlin_drawingapp.R
 import model.BackGroundColor
@@ -47,11 +53,27 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         }
 
         btnMakePhotoView.setOnClickListener {
-            getPhotoFromGallery()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    getPhotoFromGallery()
+                } else {
+                    if(shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        showContextPopupPermission()
+                    }
+                    else{
+                        requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                    }
+                }
+            } else {
+                getPhotoFromGallery()
+            }
         }
 
+
+
         mainLayout.setOnTouchListener { _, motionEvent ->
-            this.selectedCustomRectangleView= presenter.selectRectangle(motionEvent.x, motionEvent.y)
+            this.selectedCustomRectangleView =
+                presenter.selectRectangle(motionEvent.x, motionEvent.y)
             true
         }
 
@@ -69,6 +91,23 @@ class MainActivity : AppCompatActivity(), MainContract.View {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
 
         })
+    }
+
+    private fun showContextPopupPermission() {
+        AlertDialog.Builder(this).setTitle("권한이 필요합니다")
+            .setMessage("사진을 불러오기 위해 권한이 필요합니다")
+            .setPositiveButton("동의하기") { _, _ ->
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        1000
+                    )
+                }
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
+
     }
 
     private fun getPhotoFromGallery() {
@@ -107,11 +146,11 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         }
 
 
-    override fun displayRectAttribute(rgbInfo:String, opacity:Int, rect:Rect) {
+    override fun displayRectAttribute(rgbInfo: String, opacity: Int, rect: Rect) {
         val rgbValueTextView = findViewById<TextView>(R.id.tv_rgb_value)
         val opacitySeekBar = findViewById<SeekBar>(R.id.seekbar_opacity)
         rgbValueTextView.text = rgbInfo
-        opacitySeekBar.progress = (opacity/25.5).toInt()
+        opacitySeekBar.progress = (opacity / 25.5).toInt()
         rect.backGroundColor.observe(this, backgroundObserver)
         rect.opacity.observe(this, opacityObserver)
     }
