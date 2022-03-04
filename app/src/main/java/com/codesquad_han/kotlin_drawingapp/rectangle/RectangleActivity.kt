6478af04287f752674b649c589082ae46169d8ac
hanchang97 <nimok97@gmail.com>
2022-03-04 16:48:25 +0900

    feat : 갤러리 접근 구현 선택된 사각형이 있는 경우 갤러리 접근 버튼 활성화 갤러리 접근 버튼 클릭 시 권한 요청 대화상자 활성화 앱 선택기 대화상자 구현

--- app/src/main/java/com/codesquad_han/kotlin_drawingapp/rectangle/RectangleActivity.kt
+++ app/src/main/java/com/codesquad_han/kotlin_drawingapp/rectangle/RectangleActivity.kt
@@ -1,18 +1,22 @@
 package com.codesquad_han.kotlin_drawingapp.rectangle
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver
+import androidx.activity.result.contract.ActivityResultContracts
 import com.codesquad_han.kotlin_drawingapp.data.RectangleRepositoryImpl
 import com.codesquad_han.kotlin_drawingapp.databinding.ActivityRectangleBinding
 import com.codesquad_han.kotlin_drawingapp.model.Plane
 import com.codesquad_han.kotlin_drawingapp.model.Rectangle
 import com.codesquad_han.kotlin_drawingapp.model.RectangleFactory
 import com.google.android.material.slider.Slider
+import com.google.android.material.snackbar.Snackbar
 
 class RectangleActivity : AppCompatActivity(), RectangleContract.View, RectangleViewClickInterface {
 
@@ -66,7 +70,7 @@ class RectangleActivity : AppCompatActivity(), RectangleContract.View, Rectangle
 
         setBtnMakeRectangle()
         setTransparencySlider()
-
+        setBtnGallery()
     }
 
     // presenter 초기화 및 livedata 옵저버 등록
@@ -103,12 +107,18 @@ class RectangleActivity : AppCompatActivity(), RectangleContract.View, Rectangle
             binding.sliderTransparency?.let {
                 it.value = alpha.toFloat()
             }
+            binding.btnOpenGallery?.let {
+                it.isEnabled = true
+            }
 
             // id 값을 활용해 현재 선택된 사각형 투명도 데이터 업데이트 후 뷰에 반영시키기
             SELECTED_RECTANGLE_ID = id
         } else {
             binding.constraintLayoutControl?.let {
                 it.visibility = View.INVISIBLE
+            }
+            binding.btnOpenGallery?.let {
+                it.isEnabled = false
             }
         }
     }
@@ -132,6 +142,39 @@ class RectangleActivity : AppCompatActivity(), RectangleContract.View, Rectangle
                 }
 
             })
+        }
+    }
+
+    fun setBtnGallery() {
+        val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode == RESULT_OK){
+                Log.d("AppTest", "RectangleActivity/ data : ${it.data?.data}")
+                // uri 전달하기!!!!
+
+            }
+            else{
+                Snackbar.make(binding.root, "사진 불러오기 취소", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+        val requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                isGranted: Boolean ->
+                if(isGranted){
+                    val intent = Intent(Intent.ACTION_PICK)
+                    intent.type = "image/*"
+                    getContent.launch(Intent.createChooser(intent, "Gallery"))
+                }
+                else{
+                    Snackbar.make(binding.root, "갤러리 접근 권한이 승인되지 않았습니다", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+
+        binding.btnOpenGallery?.let {
+           it.setOnClickListener {
+                // 갤러리 열고 uri 가져오기 구현하기
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
         }
     }
 
