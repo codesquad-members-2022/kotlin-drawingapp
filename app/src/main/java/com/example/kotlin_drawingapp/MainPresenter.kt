dbb241d60b8e5fcd34269696c78a0decaf0d4dd5
banjjak2 <interr0bang@naver.com>
2022-03-08 20:09:10 +0900

    feat: 프레젠터에 이미지 생성 및 데이터 표현 기능 추가

--- app/src/main/java/com/example/kotlin_drawingapp/MainPresenter.kt
+++ app/src/main/java/com/example/kotlin_drawingapp/MainPresenter.kt
@@ -1,6 +1,8 @@
 package com.example.kotlin_drawingapp
 
+import android.graphics.Bitmap
 import com.example.kotlin_drawingapp.model.Color
+import com.example.kotlin_drawingapp.model.ImageFactory
 import com.example.kotlin_drawingapp.model.RectangleFactory
 import com.example.kotlin_drawingapp.model.draw.DrawObject
 import com.example.kotlin_drawingapp.model.source.DrawingDataSource
@@ -10,11 +12,13 @@ class MainPresenter(
     private val mainView: MainContract.View
 ) : MainContract.Presenter {
 
-    override fun createDrawObject(category: DrawObject.Category) {
-        when (category) {
-            DrawObject.Category.RECTANGLE -> drawingRepository.saveDrawObject(RectangleFactory.create())
-            else -> { }
-        }
+    override fun createRectangle() {
+        drawingRepository.saveDrawObject(RectangleFactory.create())
+        mainView.showDrawObject(drawingRepository.getAllDrawObject())
+    }
+
+    override fun createImage(bitmap: Bitmap) {
+        drawingRepository.saveDrawObject(ImageFactory.create(bitmap))
         mainView.showDrawObject(drawingRepository.getAllDrawObject())
     }
 
@@ -31,7 +35,7 @@ class MainPresenter(
         drawObject?.let {
             when (drawObject) {
                 is DrawObject.Rectangle -> mainView.showDrawObjectInfo(drawObject.rgb, drawObject.alpha)
-                else -> mainView.showDrawObjectInfo(Color(255, 255, 255), 10)
+                is DrawObject.Image -> mainView.showDrawObjectInfo(Color(255, 255, 255), drawObject.alpha)
             }
         }
     }
@@ -42,6 +46,7 @@ class MainPresenter(
             val replacement = tmpCurrentSelectedDrawObject
             when (replacement) {
                 is DrawObject.Rectangle -> replacement.alpha = alpha
+                is DrawObject.Image -> replacement.alpha = alpha
             }
             drawingRepository.saveCurrentSelectedDrawObject(replacement)
             drawingRepository.modifyDrawObject(tmpCurrentSelectedDrawObject, replacement)
