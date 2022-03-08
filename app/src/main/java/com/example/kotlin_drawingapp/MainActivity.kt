0b067940097e212a54ea09450f28c7f7f422182c
banjjak2 <interr0bang@naver.com>
2022-03-08 20:14:01 +0900

    feat: 사진 버튼 클릭 시 갤러리를 통해 사진을 가져와서 표현하는 기능 구현

--- app/src/main/java/com/example/kotlin_drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_drawingapp/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.kotlin_drawingapp
 
+import android.content.Intent
+import android.graphics.ImageDecoder
 import android.graphics.PointF
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin_drawingapp.model.Color
 import com.example.kotlin_drawingapp.model.draw.DrawObject
 import com.example.kotlin_drawingapp.model.draw.DrawView
-import com.example.kotlin_drawingapp.model.source.memory.PlaneDataSource
 import com.example.kotlin_drawingapp.model.source.DrawingRepository
+import com.example.kotlin_drawingapp.model.source.memory.PlaneDataSource
 
 private const val TAG = "MainActivity"
 
@@ -31,7 +36,7 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         seekBarAlpha = findViewById(R.id.seekBar)
         val btnGenerateRectangle = findViewById<Button>(R.id.btn_generate_rect)
         btnGenerateRectangle.setOnClickListener {
-            presenter.createDrawObject(DrawObject.Category.RECTANGLE)
+            presenter.createRectangle()
         }
 
         drawView.setOnTouchListener(object : DrawView.OnTouchListener {
@@ -49,6 +54,27 @@ class MainActivity : AppCompatActivity(), MainContract.View {
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
+
+        val galleryIntent = Intent()
+        galleryIntent.type = "image/*"
+        galleryIntent.action = Intent.ACTION_PICK
+        val selectImageResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data = result.data?.data
+                data?.let {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        presenter.createImage(ImageDecoder.decodeBitmap(ImageDecoder.createSource(contentResolver, it)))
+                    } else {
+                        presenter.createImage(MediaStore.Images.Media.getBitmap(contentResolver, it))
+                    }
+                }
+            }
+        }
+
+        val btnGenerateImage = findViewById<Button>(R.id.btn_generate_image)
+        btnGenerateImage.setOnClickListener {
+            selectImageResult.launch(galleryIntent)
+        }
     }
 
     override fun showDrawObject(drawObject: List<DrawObject>) {
